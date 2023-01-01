ad894ac43851676956b17ace1acdaeb80b15b599
daminoworld <ekadls757@gmail.com>
2023-01-02 04:25:13 +0900

    [Chore] Set BaseVC

--- HARA/HARA/Global/Base/BaseVC.swift
+++ HARA/HARA/Global/Base/BaseVC.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class BaseVC: UIViewController {
-
+class BaseVC: UIViewController, UIGestureRecognizerDelegate {
+    
+    // MARK: Properties
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        activityIndicator.center = view.center
+        
+        // 기타 옵션
+        activityIndicator.color = .gray
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = .medium
+        activityIndicator.stopAnimating()
+        return activityIndicator
+    }()
+    
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(activityIndicator)
+        setHaraBackGroundColor()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Custom Methods
+extension BaseVC {
+    func hideTabbar() {
+        self.tabBarController?.tabBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showTabbar() {
+        self.tabBarController?.tabBar.isHidden = false
     }
-    */
+    
+    /// 화면 터치시 키보드 내리는 메서드
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.delegate = self
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func setHaraBackGroundColor() {
+        /// 해라 기본 배경 색상으로 변경
+        view.backgroundColor = .white
+    }
+}
 
+// MARK: - Custom Methods(화면전환)
+extension BaseVC {
+    
+    /// 특정 탭의 루트 뷰컨으로 이동시키는 메서드
+    func goToRootOfTab(index: Int) {
+        tabBarController?.selectedIndex = index
+        if let nav = tabBarController?.viewControllers?[index] as? UINavigationController {
+            nav.popToRootViewController(animated: true)
+        }
+    }
 }
