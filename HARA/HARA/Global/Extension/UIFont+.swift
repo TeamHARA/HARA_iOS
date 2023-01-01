c7e0b31382f0249d6e67957b50d4fd3527a6f8f2
daminoworld <ekadls757@gmail.com>
2023-01-02 02:39:07 +0900

    [Chore] Make Fonts Constants

--- HARA/HARA/Global/Extension/UIFont+.swift
+++ HARA/HARA/Global/Extension/UIFont+.swift
@@ -5,4 +5,47 @@
 //  Created by 김담인 on 2023/01/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+
+    class var haraM1M24: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 24.0)!
+    }
+
+    class var haraM2M18: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 18.0)!
+    }
+
+    class var haraH1Sb16: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 16.0)!
+    }
+
+    class var haraH2M16: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 16.0)!
+    }
+
+    class var haraB1Sb14: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
+    }
+
+    class var haraB2M14: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 14.0)!
+    }
+
+    class var haraB3R14: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14.0)!
+    }
+
+    class var haraSub1Sb12: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 12.0)!
+    }
+
+    class var haraSub2M12: UIFont {
+        return UIFont(name: "Pretendard-Medium", size: 12.0)!
+    }
+
+    class var haraSub3R12: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 12.0)!
+    }
+}
