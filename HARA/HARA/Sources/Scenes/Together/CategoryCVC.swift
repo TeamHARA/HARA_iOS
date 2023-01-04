5f284a68b78c1e7b94fe2689ade8986651856526
daminoworld <ekadls757@gmail.com>
2023-01-04 15:28:58 +0900

    [Feat] #3-Category UI

---
+++ HARA/HARA/Sources/Scenes/Together/CategoryCVC.swift
@@ -0,0 +1,66 @@
+//
+//  CategoryCVC.swift
+//  HARA
+//
+//  Created by 김담인 on 2023/01/04.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class CategoryCVC: UICollectionViewCell {
+    
+    // MARK: - Properties
+    let categoryLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.font = .haraH2M16
+        $0.textColor = .hBlue2
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                contentView.backgroundColor = .hBlue1
+                categoryLabel.textColor = .hWhite
+                contentView.layer.borderWidth = 0
+            } else {
+                contentView.backgroundColor = .hWhite
+                contentView.layer.borderColor = UIColor.hBlue2.cgColor
+                contentView.layer.borderWidth = 1
+                categoryLabel.textColor = .hBlue2
+            }
+        }
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        contentView.addSubview(categoryLabel)
+        
+        categoryLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - UI
+    private func setUI() {
+        contentView.backgroundColor = .hWhite
+        contentView.makeRounded(cornerRadius: 8)
+    }
+    
+    // MARK: - Function
+    func setData(data: String) {
+        categoryLabel.text = data
+    }
+}
