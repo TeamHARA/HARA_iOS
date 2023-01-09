9a17385c8b8e2062cfe8c9275653dd1b3bed9337
BaeSungho <bacho4235@gmail.com>
2023-01-09 14:50:39 +0900

    [Feat] #16-1, 4, 5번째 writeStepView 수정사항 반영 및 누락된 파일 추가

--- HARA/HARA/Sources/Scenes/Write/WriteVC.swift
+++ HARA/HARA/Sources/Scenes/Write/WriteVC.swift
@@ -16,12 +16,12 @@ class WriteVC: UIViewController {
     private let background = UIImageView().then {
         $0.image = UIImage(named: "background")
         $0.contentMode = .scaleToFill
-        $0.backgroundColor = .red
+        $0.backgroundColor = .clear
     }
     
     private let closeButton = UIButton().then {
-        $0.setBackgroundImage(UIImage(named: "icn_close"), for: .normal)
-        $0.contentMode = .scaleAspectFit
+        $0.setBackgroundImage(UIImage(named: "closeBtn"), for: .normal)
+        $0.contentMode = .scaleToFill
     }
     
     private let titleLabel = UILabel().then {
@@ -61,18 +61,28 @@ class WriteVC: UIViewController {
         $0.contentMode = .scaleAspectFit
     }
     
+    private let uploadButton = UIButton().then {
+        $0.backgroundColor = .clear
+        $0.layer.cornerRadius = 25
+        $0.layer.borderWidth = 1.0
+        $0.layer.borderColor = UIColor.hBlue3.cgColor
+        $0.titleLabel?.font = .haraH1Sb16
+        $0.setTitle("업로드하기", for: .normal)
+        $0.setTitleColor(.hBlue3, for: .normal)
+        $0.isUserInteractionEnabled = false
+    }
+    
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = .clear
-        enabledArrowBtn()
+        vc5.delegate = self
+        disableArrowButton()
         setLayout()
         setPress()
         setupDelegate()
-        if let firstVC = viewList.first {
-            pageViewController.setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
-        }
+        setFirstPageView()
     }
     
     // MARK: - Function
@@ -88,16 +98,19 @@ class WriteVC: UIViewController {
             /// 화면 이동 (지금 페이지에서 -1 페이지로 setView 합니다.)
             self.pageViewController.setViewControllers([self.viewList[prevPage]], direction: .reverse, animated: true)
             /// 버튼눌렸을 때 페이지 변경 애니메이션 적용
-            self.enabledArrowBtn()
-            self.progressBarAnimate()
+            self.disableArrowButton()
+            self.makeProgressBarAnimate()
         }
         nextButton.press {
             let nextPage = self.currentPage + 1
             /// 화면 이동 (지금 페이지에서 +1 페이지로 setView 합니다.)
             self.pageViewController.setViewControllers([self.viewList[nextPage]], direction: .forward, animated: true)
             /// 버튼눌렸을 때 페이지 변경 애니메이션 적용
-            self.enabledArrowBtn()
-            self.progressBarAnimate()
+            self.disableArrowButton()
+            self.makeProgressBarAnimate()
+        }
+        uploadButton.press {
+            self.dismiss(animated: true, completion: nil)
         }
     }
     
@@ -107,20 +120,20 @@ class WriteVC: UIViewController {
     }
     
     /// 페이지 이동 버튼 제어 해주는 함수
-    private func enabledArrowBtn() {
+    private func disableArrowButton() {
         if currentPage == 0 {
-            prevButton.layer.isHidden = true
+            prevButton.isHidden = true
         } else if currentPage == 4 {
-            nextButton.layer.isHidden = true
+            nextButton.isHidden = true
         } else {
-            prevButton.layer.isHidden = false
-            nextButton.layer.isHidden = false
+            prevButton.isHidden = false
+            nextButton.isHidden = false
             nextButton.isEnabled = true
             prevButton.isEnabled = true
         }
     }
     
-    private func progressBarAnimate() {
+    private func makeProgressBarAnimate() {
         UIView.animate(withDuration: 0.1, animations: {
             self.animateView.alpha = 1
             self.animateView.backgroundColor = .hBlue4
@@ -130,11 +143,16 @@ class WriteVC: UIViewController {
             self.animateView.backgroundColor = .clear
         })
     }
+    
+    private func setFirstPageView() {
+        if let firstVC = viewList.first {
+            pageViewController.setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
 }
 
 // MARK: - Layout
 extension WriteVC{
-    
     private func setLayout(){
         self.view.insertSubview(background, at: 0)
         
@@ -145,7 +163,7 @@ extension WriteVC{
         view.addSubViews([closeButton, titleLabel])
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
-        pageViewController.view.addSubviews([prevButton, nextButton, animateView])
+        pageViewController.view.addSubviews([prevButton, uploadButton, nextButton, animateView])
         
         
         closeButton.snp.makeConstraints{
@@ -179,6 +197,13 @@ extension WriteVC{
             $0.height.equalTo(20.adjustedH)
         }
         
+        uploadButton.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(prevButton)
+            $0.width.equalTo(210.adjustedW)
+            $0.height.equalTo(46.adjustedH)
+        }
+        
         nextButton.snp.makeConstraints{
             $0.trailing.equalTo(self.view.safeAreaLayoutGuide).offset(-35.adjustedW)
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(-60.adjustedH)
@@ -192,7 +217,7 @@ extension WriteVC{
 extension WriteVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        progressBarAnimate()
+        makeProgressBarAnimate()
         guard let index = viewList.firstIndex(of: viewController) else { return nil }
         let previousIndex = index - 1
         if previousIndex < 0 {
@@ -202,7 +227,7 @@ extension WriteVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate 
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        progressBarAnimate()
+        makeProgressBarAnimate()
         guard let index = viewList.firstIndex(of: viewController) else { return nil }
         let nextIndex = index + 1
         if nextIndex == viewList.count {
@@ -213,23 +238,50 @@ extension WriteVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate 
     
     /// 버튼 클릭시에만 enable을 적용되는 것을 방지하기 위해, page를 스크롤해서 이동시킬 때에서 enable효과를 주기 위한 "didFinishAnimating"
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        enabledArrowBtn()
-        progressBarAnimate()
+        disableArrowButton()
+        makeProgressBarAnimate()
     }
 }
 
+// MARK: - UITextFieldDelegate
 extension WriteVC: UITextFieldDelegate {
     /// ✅ textField 에서 편집을 시작한 후
     func textFieldDidBeginEditing(_ textField: UITextField) {
         /// 키보드 업
         textField.becomeFirstResponder()
         /// 입력 시 textField 를 강조하기 위한 테두리 설정
-        textField.layer.borderWidth = 2
+        textField.layer.borderWidth = 1
         textField.layer.borderColor = UIColor.red.cgColor
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
 
-
+// MARK: - SendIsclickedDelegate
+/// 5번째 페이지에서 버튼이 선택되었을 때 업로드 버튼 활성화
+extension WriteVC: SendIsclickedDelegate{
+    func SendIsclicked(alone: Bool, everyone: Bool) {
+        if alone == true || everyone == true{
+            /// 1.  업로드 버튼 활성화
+            uploadButton.isUserInteractionEnabled = true
+            /// 2.  버튼 디자인 변경
+            uploadButton.setTitleColor(.white, for: .normal)
+            uploadButton.backgroundColor = .hBlue1
+            uploadButton.layer.borderColor = UIColor.hBlue1.cgColor
+        }
+        else{
+            // 1. 업로드 버튼 비활성화
+            uploadButton.isUserInteractionEnabled = false
+            // 2. 버튼 디자인 변경
+            uploadButton.setTitleColor(.hBlue3, for: .normal)
+            uploadButton.backgroundColor = .clear
+            uploadButton.layer.borderColor = UIColor.hBlue3.cgColor
+        }
+    }
+}
 
 
 
